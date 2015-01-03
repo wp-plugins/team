d6f91f558250398ca45b31afbefbad0134e7bd2b
paratheme <paratheme@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-03 04:49:06 +0000

    file update
    git-svn-id: https://plugins.svn.wordpress.org/team/trunk@1058856 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- team.php
+++ team.php
@@ -84,31 +84,7 @@ function team_activation()
 		update_option('team_customer_type', $team_customer_type); //update plugin version.
 		
 		
-		
-		
-		
 
-		$api_url = 'http://paratheme.com/installstats/';
-		
-		$wp_version = get_bloginfo('version'); // no change
-		$domain = get_bloginfo( 'url' ); // no change
-		$item_slug = basename(dirname(__FILE__)); // no change
-		$item_version = '1.3'; // current item version
-		$item_type = 'plugin'; // plugin, theme, addon		
-		$action = 'active'; //active, inactive, install, uninstall
-	
-		$request_string = array(
-				'user-agent' => $wp_version . '; ' . $domain . '; ' . $item_slug . '; ' . $item_version . '; ' . $item_type. '; ' . $action,
-
-				
-			);
-			
-			
-		wp_remote_post($api_url, $request_string);
-		
-		
-		
-		
 		
 		
 		
